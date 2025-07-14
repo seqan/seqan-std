cd447b65185187e144f1002fe2ed7c309cae281f
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2025-07-14 13:37:09 +0200

    fix: add missing declaration for __int128 types

--- chunk_view.hpp
+++ chunk_view.hpp
@@ -12,6 +12,7 @@
 #define SEQAN_STD_CHUNK_VIEW
 
 #include <cassert>
+#include <cstdint>
 #include <ranges>
 
 #ifdef __cpp_lib_ranges_chunk
@@ -66,6 +67,24 @@ constexpr auto to_unsigned_like(std::_Unsigned128 v) noexcept
 }
 #endif
 
+//!WORKAROUND GCC/Clang and libstdc++
+// GCC uses __int128 as range type of example std::ranges::iota_view<size_t>
+// For this we require a conversion function, that
+// does not mention __int128, otherwise -pedantic will complain
+#if defined(__GNUC__) && !defined(_LIBCPP_VERSION)
+template <typename T>
+    requires (!std::integral<T> && requires(T v) { std::ranges::__detail::__to_unsigned_like(v); })
+constexpr auto to_unsigned_like(T v) noexcept
+{
+    return std::ranges::__detail::__to_unsigned_like(v);
+}
+template <typename T>
+    requires (!std::integral<T> && requires(T v) { std::ranges::__detail::__to_signed_like(v); })
+constexpr auto to_signed_like(T v) noexcept
+{
+    return std::ranges::__detail::__to_signed_like(v);
+}
+#endif
 
 } // namespace seqan::stl::detail::chunk
 
